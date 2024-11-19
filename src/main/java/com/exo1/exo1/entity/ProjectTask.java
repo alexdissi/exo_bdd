8e1887e4b26fce37c2c7e7f060c1232506e806b0
Alexandre Dissi <alexandredissi@icloud.com>
2024-11-19 16:51:56 +0100

    Add Project

--- src/main/java/com/exo1/exo1/entity/ProjectTask.java
+++ src/main/java/com/exo1/exo1/entity/ProjectTask.java
@@ -1,6 +1,14 @@
 package com.exo1.exo1.entity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "project_task")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProjectTask {
     @Id
     @Column(name = "projet_id")
